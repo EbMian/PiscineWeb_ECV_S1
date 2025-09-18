f1b13e795d890dc6ece8124ea9a1cf1336d21a3b
Normi029-RTHERY <rom.t95@free.fr>
2025-09-18 10:13:47 +0200

    format

--- JS/panier.js
+++ JS/panier.js
@@ -1,10 +1,10 @@
 const cart = {}
-const product = {taille, finition, couleur, prix}
+const product = { taille, finition, couleur, prix }
 const pro = {}
 const option = {}
 const productList = {}
 
-function calculatePrice (size, finition, couleur, basePrice) {
+function calculatePrice(size, finition, couleur, basePrice) {
 
 }
 
@@ -20,29 +20,29 @@ function calculatePrice (size, finition, couleur, basePrice) {
 
 
 "options" = [
-    {colors: [{red: 2}, {orange: 2}, {white: 1}, {black: 1}, {green: 2}]},
-    {size: [{small: 1}, {medium: 2}, {large: 3}]},
-    {finition: [{mate: 1}, {shiny: 3}]},
-    {graphics: [{image: 3}]},
-    {giftOrNot: [{gift: 2}, {notGift: 1}]}
+    { colors: [{ red: 2 }, { orange: 2 }, { white: 1 }, { black: 1 }, { green: 2 }] },
+    { size: [{ small: 1 }, { medium: 2 }, { large: 3 }] },
+    { finition: [{ mate: 1 }, { shiny: 3 }] },
+    { graphics: [{ image: 3 }] },
+    { giftOrNot: [{ gift: 2 }, { notGift: 1 }] }
 ];
 
 const shoppingListe = document.getElementById('liste');
 console.log(shoppingListe);
- 
+
 const articleTemplate = document.getElementById('liste-item');
 console.log(articleTemplate);
- 
+
 const produits = ["café", "thé", "mug", "chocolat"]
- 
+
 produits.forEach(produit => {
- 
+
     const clone = document.importNode(articleTemplate.content, true);
- 
+
     clone.querySelector('.nom').textContent = produit;
     clone.querySelector('.quantite').textContent = produit;
     clone.querySelector('.nom').options = produit;
     clone.querySelector('.prix').textContent = produit;
- 
+
     shoppingListe.appendChild(clone)
 })
