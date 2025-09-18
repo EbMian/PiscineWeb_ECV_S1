576885d7e40772df16b8e47010fa563752704670
Normi029-RTHERY <rom.t95@free.fr>
2025-09-18 21:16:08 +0200

    modfis

--- components/footer/footer.js
+++
@@ -1,5 +0,0 @@
-const footer = document.getElementById('footer');
-const templateFooter = document.getElementById('custom-footer');
-console.log(footer);
-const clone = document.importNode(templateFooter.content, true);
-footer.appendChild(clone);
