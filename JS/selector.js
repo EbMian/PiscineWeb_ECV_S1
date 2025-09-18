dee48ee7205c0f77d8aeffb50096142b08637ddb
Normi029-RTHERY <rom.t95@free.fr>
2025-09-18 11:39:29 +0200

    local storage support

--- JS/selector.js
+++ JS/selector.js
@@ -1,26 +1,28 @@
 //* Gestion de la sélection des paramètrees de customization
 
-let selectedSize;
-let selectedColor;
-let selectedFinish;
+let selectedSize = sessionStorage.getItem('selectedSize') || undefined;
+let selectedColor = sessionStorage.getItem('selectedColor') || undefined;
+let selectedFinish = sessionStorage.getItem('selectedFinish') || undefined;
 
 const sizeSelectors = document.querySelectorAll('input[name="size-setting"]');
 const colorSelectors = document.querySelectorAll('input[name="color-setting"]');
 const finishSelectors = document.querySelectorAll('input[name="finish-setting"]');
 
-
 const updateSelectedSize = () => {
     selectedSize = document.querySelector('input[name="size-setting"]:checked')?.dataset.value;
+    sessionStorage.setItem('selectedSize', selectedSize ?? '');
     updatePriceElement();
 };
 
 const updateSelectedColor = () => {
     selectedColor = document.querySelector('input[name="color-setting"]:checked')?.dataset.value;
+    sessionStorage.setItem('selectedColor', selectedColor ?? '');
     updatePriceElement();
 };
 
 const updateSelectedFinish = () => {
     selectedFinish = document.querySelector('input[name="finish-setting"]:checked')?.dataset.value;
+    sessionStorage.setItem('selectedFinish', selectedFinish ?? '');
     updatePriceElement();
 };
 
@@ -54,6 +56,22 @@ const updatePriceElement = () => {
     const priceElement = document.querySelector('#price');
     priceElement.innerText = `${getPrice(false)},00 €`;
 };
+
+
+//* Initialisation des sélections à partir du sessionStorage
+
+if (selectedSize) {
+    const el = document.querySelector(`input[name="size-setting"][data-value="${selectedSize}"]`);
+    if (el) el.checked = true;
+}
+if (selectedColor) {
+    const el = document.querySelector(`input[name="color-setting"][data-value="${selectedColor}"]`);
+    if (el) el.checked = true;
+}
+if (selectedFinish) {
+    const el = document.querySelector(`input[name="finish-setting"][data-value="${selectedFinish}"]`);
+    if (el) el.checked = true;
+}
 
 updateSelectedSize();
 updateSelectedColor();
